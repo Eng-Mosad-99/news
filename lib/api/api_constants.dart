0e67c7c4a86d369847a91536f9b97d7eca81c32c
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-03-21 04:01:18 -0400

    source api implementation part 1

---
+++ lib/api/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants {
+  static const String baseUrl = 'newsapi.org';
+  static const String apiKey = '593ed4ed895f48c3b19ee41019a0435b';
+}
--- lib/api/api_manager.dart
+++ lib/api/api_manager.dart
@@ -1,1 +1,13 @@
-class ApiManager {}
+import 'package:http/http.dart' as http;
+import 'package:news/api/api_constants.dart';
+import 'package:news/api/end_points.dart';
+
+class ApiManager {
+  // https://newsapi.org/v2/top-headlines/sources?
+  static void getSources(String categoryId) async {
+    Uri url = Uri.https(ApiConstants.baseUrl, EndPoints.sourceApi, {
+      'apiKey': ApiConstants.apiKey,
+    });
+    http.get(url);
+  }
+}
