06f07bc50901d4b1087c6f4f2394bd9b38147c64
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-03-22 04:35:56 -0400

    add HomeDrawer widget and integrate it into HomeScreen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news/home/category_details/category_details.dart';
 import 'package:news/home/category_fragment/category_fragment.dart';
+import 'package:news/home/drawer/home_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,6 +12,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title:  Text('News App' , style: Theme.of(context).textTheme.headlineLarge),
       ),
+      drawer: HomeDrawer(),
       body: CategoryFragment(),
     );
   }
